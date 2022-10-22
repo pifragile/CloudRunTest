65897db0acb23b3db90d3a6faa28320c0a8c0d17
pifragile <pigu@bluewin.ch>
2022-10-22 19:47:30 +0800

    init apptest

--- main.py
+++ main.py
@@ -1,14 +1,1 @@
-import os
-
-from flask import Flask, request
-
-app = Flask(__name__)
-
-
-@app.route("/", methods=['GET'])
-def hello_world():
-    return "Hello..... {}!".format(request.args['contract'])
-
-
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+print('yalla', flush=True)
